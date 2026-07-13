48d6dd51585d43896a0c0f3c1fc2f46df2f2e41b
Raheelkhan-05 <2005raheelkhan@gmail.com>
2026-07-13 15:53:40 +0530

    Added Landing Page UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
-import LandingPage from "./Pages/LandingPage";
+import LandingPage from "./pages/LandingPage";
 
 
 function App() {
