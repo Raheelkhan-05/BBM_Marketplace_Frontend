3c6cc6fb2685a46b61cf6e49b882715c36fd40fd
Raheelkhan-05 <2005raheelkhan@gmail.com>
2026-09-09 17:21:12 +0530

    Removed the Home Nav as then logo button also working the same

--- src/components/navItems.js
+++ src/components/navItems.js
@@ -15,14 +15,7 @@ import {
 
 export function NAV_ITEMS({ isLoggedIn, isApprovedSeller, onOpenRfq, navigate }) {
     return [
-        isLoggedIn ? {
-            id: "home",
-            label: "Home",
-            icon: Home,
-            to: "/home",
-            onClick: () => navigate("/home"),
-            match: (p) => p === "/home",
-        } : null,
+
         isLoggedIn
             ? {
                 id: "myproducts",
